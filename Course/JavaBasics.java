f2b2149b37c3f3e69f1f206715eb88a74feb1906
ChandanP07 <chandan019283@gmail.com>
2024-08-17 12:29:40 +0530

    Add JavaBasics

--- Course/JavaBasics.java
+++ Course/JavaBasics.java
@@ -1,1 +1,99 @@
+import java.util.*;
 
+public class JavaBasics {
+    public static void main(String[] args) {
+        // System.out.println("*@@*"); //ln- line ka space de dega
+        // System.out.println("***"); //ln- line ka space de dega
+        // System.out.println("**"); //ln- line ka space de dega
+        // System.out.println("*"); //ln- line ka space de dega
+
+        
+        // int a = 10;
+        // System.out.println(a);
+        // String name = "Tony Stark";
+        // System.out.println(name);
+
+        
+        // byte b=8;
+        // System.out.println(b);
+        // char ch='a';
+        // System.out.println(ch);
+
+
+
+        // code to calculate sum
+        // int a=10;
+        // int b=5;
+        // int sum=a+b;
+        // System.out.println(sum);
+
+        Scanner sc = new Scanner(System.in);
+        // String name=sc.next();
+        // System.out.println(name);
+
+        // String name=sc.nextLine();
+        // System.out.println(name);
+
+        // int num=sc.nextInt();
+        // System.out.println(num);
+
+        // float price=sc.nextFloat();
+        // System.out.println(price);
+
+        // boolean p=sc.nextBoolean();
+        // System.out.println(p);
+
+
+
+        // Area of circle
+        // float radius = sc.nextFloat();
+        // float area = 3.14f * radius * radius;
+        // System.out.println(area);
+
+
+
+        // Type Conversion
+        // int a = 25;
+        // long b = a;
+        // long a=25;
+        // int b=a;
+        // System.out.println(b);
+        // float num=sc.nextInt();
+        // System.out.println(num);
+
+
+
+        // Typecasting
+        // float a=55.12f;
+        // int b=(int)a;
+        // System.out.println(b);
+        // char ch = 'a';
+        // char ch2 = 'b';
+        // int number = ch;
+        // int number2 = ch2;
+        // System.out.println(number);
+        // System.out.println(number2);
+
+
+
+        // Type Promotion
+        // char a='a';
+        // char b='b';
+        // System.out.println((int)(b));
+        // System.out.println((int)(a));
+        // System.out.println(b-a);
+        // int a = 45;
+        // long b = 56;
+        // float c = 99.08f;
+        // double d = 67.89;
+        // int ans = a + b + c + d;
+        // System.out.println(ans);
+        byte b = 5;
+        byte a = (byte) (b * 2);
+        System.out.print(a);
+
+
+        sc.close();
+    }
+}
+// boilerplate code
