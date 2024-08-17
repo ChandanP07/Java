813dd811c781cb8d7c151870a14fc34a595166f1
ChandanP07 <chandan019283@gmail.com>
2024-08-17 12:32:52 +0530

    Add JavaBasics2

---
+++ Course/JavaBasics2.java
@@ -0,0 +1,40 @@
+public class JavaBasics2 {
+    public static void main(String[] args) {
+
+        // System.out.println("add = " +(A+B));
+        // Arthimetic Unary Operator
+        // int a=10;
+        // int b=--a;       //predecrement
+        // int c=a--;       //postdecrement
+        // System.out.println(a);
+        // System.out.println(c);
+
+
+        // Relational operator
+        // int a=10;
+        // int b=9;
+        // System.out.println((a!=b));
+        // System.out.println((a>b));
+        // System.out.println((a<b));
+        // System.out.println((a>=b));
+        // System.out.println((a<=b));
+
+
+        // Logical Operator
+        // System.out.println((3>2)&&(5>9));
+        // System.out.println(!(3>4));
+
+
+        // Assignment Operator
+        // int A = 10;
+        // int B = 5;
+        // System.out.println(A+=10);
+        // // System.out.println(B-=5);
+        // System.out.println(B*=5);
+        // System.out.println(B/=5);
+        // System.out.println(B%=5);
+
+
+    }
+
+}
